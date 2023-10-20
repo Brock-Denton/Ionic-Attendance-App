866830daab22bc9503a0b19f6109e6b7f78655d3
Brock-Denton <brockchecksmail@gmail.com>
2023-10-20 12:54:54 -0400

    Add files via upload

---
+++ app/roster/roster.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Student, StudentsService } from '../students.service';
+
+@Component({
+  selector: 'app-roster',
+  templateUrl: './roster.page.html',
+  styleUrls: ['./roster.page.scss'],
+})
+export class RosterPage implements OnInit {
+  students: Student[] = [];
+
+constructor(
+  private studentService: StudentsService,) { }
+
+ngOnInit() {
+  this.students = this.studentService.getAll();
+}
+
+}
